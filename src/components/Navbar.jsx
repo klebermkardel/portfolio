5187e18f06d4d1afab62c7ee1bf75aebe746c3d5
klebermkardel <molinakardel@outlook.com.br>
2026-02-14 09:49:48 -0300

    feat: cria responsividade da barra de navegação para todos os tamanhos de telas e menu hamburger para tablets e celulares

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; // Importe estes dois
 
 const Navbar = () => {
+    const [active, setActive] = useState(false);
+
+    const toggleMenu = () => {
+        setActive(!active);
+    };
+
     return (
-        <header>
+        <header className="header">
             <nav className='navBar' aria-label='Navegação Principal'>
                 <div className="logo">
                     <a href="/" className="logoName">&lt; /Kleber M Kardel &gt;</a>
                 </div>
 
-                <div className="menu">
+                {/* Ícone Hambúrguer */}
+                <div className={`mobile-menu-icon ${active ? 'active' : ''}`} onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={active ? faTimes : faBars} />
+                </div>
+
+                {/* Menu com classe condicional */}
+                <div className={`menu ${active ? 'active' : ''}`}>
                     <ul className="menuList">
-                        <li className="itemMenu"><a href="#inicio">Início</a></li>
-                        <li className="itemMenu"><a href="#sobre">Sobre</a></li>
-                        <li className="itemMenu"><a href="#habilidades">Habilidades</a></li>
-                        <li className="itemMenu"><a href="#projetos">Projetos</a></li>
-                        <li className="itemMenu"><a href="#contato">Contato</a></li>
+                        <li className="itemMenu"><a href="#inicio" onClick={toggleMenu}>Início</a></li>
+                        <li className="itemMenu"><a href="#sobre" onClick={toggleMenu}>Sobre</a></li>
+                        <li className="itemMenu"><a href="#habilidades" onClick={toggleMenu}>Habilidades</a></li>
+                        <li className="itemMenu"><a href="#projetos" onClick={toggleMenu}>Projetos</a></li>
+                        <li className="itemMenu"><a href="#contato" onClick={toggleMenu}>Contato</a></li>
                     </ul>
                 </div>
 
